b19d6fb7afc06bbed5d7a9dc813f42d26527f4b0
ayodeji <ayodejiakintobi1@gmail.com>
2023-09-15 15:04:50 +0100

    toggle-menu added

--- main.js
+++ main.js
@@ -1,12 +1,21 @@
-var mobileMenu = document.getElementById('mobile-menu');
+const menuIcon = document.getElementById('menu-icon');
+const navLinks = document.getElementById('nav-links');
+const bar1 = document.getElementById('bar1');
+const bar2 = document.getElementById('bar2');
+const bar3 = document.getElementById('bar3');
 
-mobileMenu.style.maxHeight = "0px";
+menuIcon.addEventListener('click', function() {
+  menuIcon.classList.toggle('is-active');
+  navLinks.classList.toggle('show-nav'); 
 
-function togglemenu() {
-    if (mobileMenu.style.maxHeight == "0px") {
-        mobileMenu.style.maxHeight = "200px";
-        mobileMenu.style.display = "block";
-    } else {
-        mobileMenu.style.maxHeight = "0px";
-    }
-}
+  bar1.classList.toggle('rotate-bar1');
+  bar2.classList.toggle('hide-bar2');
+  bar3.classList.toggle('rotate-bar3');
+
+ 
+  if (navLinks.classList.contains('show-nav')) {
+    document.body.style.overflow = 'hidden';
+  } else {
+    document.body.style.overflow = '';
+  }
+});
